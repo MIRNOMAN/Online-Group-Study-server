6591287b05986147d424c589acdd0c2d4149d143
MIRNOMAN <you@example.comabdullahalnoman1509@gmail.com>
2023-11-06 04:04:07 +0600

    create assignment post and get method and assignment update operation

--- index.js
+++ index.js
@@ -113,6 +113,40 @@ async function run() {
           res.send(result);
         })
 
+        // update assignment
+
+        app.get('/updates/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = {_id: new ObjectId(id)}
+            const result = await assignmentCollection.findOne(query)
+            res.send(result)
+          })
+      
+      
+      
+          app.put('/updates/:id', async (req, res) =>{
+            const id = req.params.id;
+            const filter = {_id: new ObjectId(id)}
+            const options = {upsert: true}
+            const updateAsgmt = req.body;
+            const update = {
+              $set :{
+               
+                title: updateAsgmt.title,
+                level: updateAsgmt.level,
+                marks: updateAsgmt.marks,
+                date: updateAsgmt.date,
+                description: updateAsgmt.description,
+                photo: updateAsgmt.photo
+              
+                
+              }
+            }
+            const result = await assignmentCollection.updateOne(filter, update, options)
+            res.send(result)
+          })
+      
+
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
